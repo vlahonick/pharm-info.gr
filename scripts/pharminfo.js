172be6bf886002d37dca77b17a393beb5ced885a
rootatwc <rootatwc@gmail.com>
2012-01-10 21:43:29 +0200

    added TinyMCE files

---
+++ scripts/pharminfo.js
@@ -0,0 +1,45 @@
+(function($) {
+  $(document).ready( function () {
+    $( "#el-tabs" ).tabs({
+      ajaxOptions: {
+        error: function( xhr, status, index, anchor ) {
+          $( anchor.hash ).html(
+            "Couldn't load this tab. We'll try to fix this as soon as possible. " +
+            "If this wouldn't be a demo." );
+        }
+      }
+    });
+    $( "#en-tabs" ).tabs();
+    $( ".tabs-bottom .ui-tabs-nav, .tabs-bottom .ui-tabs-nav > *" )
+			.removeClass( "ui-corner-all ui-corner-top" )
+			.addClass( "ui-corner-bottom" );
+      
+    var availableTags = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+		$( "#sb" ).autocomplete({
+			source: availableTags
+		});
+  });
+})(jQuery)
