8a486e9ea71e6ba3badea22851d016f9dc07c081
vlahonick <vlahonick@gmail.com>
2012-01-19 22:56:36 +0200

    lol2

--- application/logs/2012/01/19.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2012-01-19 22:53:03 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: css/images/ui-bg_highlight-hard_100_f5f3e5_1x100.png ~ SYSPATH/classes/kohana/request.php [ 1126 ]
-2012-01-19 22:53:03 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: css/images/ui-bg_highlight-hard_100_f5f3e5_1x100.png ~ SYSPATH/classes/kohana/request.php [ 1126 ]
---
-#0 /var/www/index.php(109): Kohana_Request->execute()
-#1 {main}
